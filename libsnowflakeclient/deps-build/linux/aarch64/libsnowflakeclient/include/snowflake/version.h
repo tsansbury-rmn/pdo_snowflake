4a028caa64421ce72805cb0ad58fb6b9d85c44b5
Tanner Sansbury <tsansbury-rmn@users.noreply.github.com>
2023-03-02 11:21:25 -0600

    feat: add linux/x64_64 prebuild library

---
+++ libsnowflakeclient/deps-build/linux/aarch64/libsnowflakeclient/include/snowflake/version.h
@@ -0,0 +1,10 @@
+/*
+ * Copyright (c) 2018-2019 Snowflake Computing, Inc. All rights reserved.
+ */
+
+#ifndef SNOWFLAKE_CLIENT_VERSION_H
+#define SNOWFLAKE_CLIENT_VERSION_H
+
+#define SF_API_VERSION "0.6.18"
+
+#endif /* SNOWFLAKE_CLIENT_VERSION_H */
